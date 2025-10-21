fe7dbf044aff873df1c45ff16f27bb19e7f4142d
pinkyj81 <yujin_pyj@daum.net>
2025-10-21 16:33:33 +0900

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         "mssql+pyodbc://pinkyj81:zoskek38!!@ms1901.gabiadb.com/yujincast?"
-        "driver=ODBC+Driver+17+for+SQL+Server"
+        "driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes"
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
